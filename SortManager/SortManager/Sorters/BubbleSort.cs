711be013fd6f7fb06ebd17b29f2c89fb5d1369b7
Luke Pinder <yelllukesky@gmail.com>
2023-03-15 12:28:41 +0000

    Implement bubblesort and test

--- SortManager/SortManager/Sorters/BubbleSort.cs
+++ SortManager/SortManager/Sorters/BubbleSort.cs
@@ -4,9 +4,17 @@
     {
         public override string Name => "BubbleSort";
 
-        public override void Sort(int[] array)
+        public override int[] Sort(int[] array)
         {
-            throw new NotImplementedException();
+            int swaps = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] <= array[i + 1]) continue;
+                (array[i], array[i + 1]) = (array[i + 1], array[i]);
+                swaps++;
+            }
+            if (swaps > 0) return Sort(array);
+            return array;
         }
     }
 }
---
+++ SortManager/SortManagerTests/BubbleSortTests.cs
@@ -0,0 +1,14 @@
+﻿using SortManager;
+using SortManager.Sorters;
+namespace SortManagerTests
+{
+    internal class BubbleSortTests
+    {
+        [TestCase(new int[] {3, 2, 1}, new int[] {1, 2, 3})]
+        public void ReturnsExpectedArray(int[] input, int[] expected)
+        {
+            BubbleSort sorter = new();
+            Assert.That(sorter.Sort(input), Is.EqualTo(expected));
+        }
+    }
+}
