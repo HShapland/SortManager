9a8ace7a72afe99d0c2cfb833cc094f076b32b35
Byron Esson <byronesson767@gmail.com>
2023-03-15 15:13:55 +0000

    add CountSort

--- SortManager/SortManager/Sorters/CountSort.cs
+++ SortManager/SortManager/Sorters/CountSort.cs
@@ -6,13 +6,25 @@ public class CountSort : Sorter
 
     public override int[] Sort(int[] array)
     {
-        int[] numCounts = new int[500];
+        int[] numCounts = new int[3];
 
-        foreach(int i in array)
+        foreach (int i in array)
         {
             numCounts[i - 1]++;
         }
+        
+        int index = 0;
 
+        for(int i = 0; i < numCounts.Length; i++)
+        {
+            while (numCounts[i] > 0)
+            {
+                array[index] = i+1;
+                index++;
+                numCounts[i]--;
+            }
+        }
 
+        return array;
     }
 }
--- SortManager/SortManagerTests/CountSortTests.cs
+++ SortManager/SortManagerTests/CountSortTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SortManager.Sorters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace SortManagerTests
 {
     internal class CountSortTests
     {
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        public void ReturnsExpectedArray(int[] input, int[] expected)
+        {
+            CountSort sorter = new();
+            Assert.That(sorter.Sort(input), Is.EqualTo(expected));
+        }
     }
 }
