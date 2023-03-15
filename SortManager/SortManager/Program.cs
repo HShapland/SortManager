53bae7f3bf21ad8af3f54d5c793d2f95e7080c99
Byron Esson <byronesson767@gmail.com>
2023-03-15 13:44:01 +0000

    add GenerateArray and stopwatch

--- SortManager/SortManager/Program.cs
+++ SortManager/SortManager/Program.cs
@@ -6,9 +6,22 @@ internal class Program
 {
     static void Main()
     {
+        Title();
         string methodChoice = TakeMethodChoice();
 
         Sorter sorter = new Factory().CreateSorter(methodChoice);
+
+        int length = TakeLengthChoice();
+
+        var watch = new System.Diagnostics.Stopwatch();
+
+        int[] randomArray = GenerateArray(length);
+
+        watch.Start();
+        int[] sortedArray = sorter.Sort(randomArray);
+        watch.Stop();
+
+
     }
 
     public static void WriteLine(string input)
@@ -43,5 +56,52 @@ internal class Program
         string[] choices = { "bubblesort", "mergesort" };
 
         return choices.Contains(choice.ToLower());  
+    }
+
+    static int TakeLengthChoice()
+    {
+        WriteLine("Enter array length:");
+        string input = Console.ReadLine();
+        
+        if(Int32.TryParse(input, out int length)) return length;
+
+        WriteLine("Must be an integer");
+        return TakeLengthChoice();
+        
+    }
+
+    static int[] GenerateArray(int length)
+    {
+        int[] ints= new int[length];
+        Random rand = new Random();
+        for(int i =0; i< length; i++)
+        {
+            ints[i]= rand.Next();
+        }
+        WriteLine(String.Join(' ', ints));
+        return ints;
+    }
+    public static void Title()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Clear();
+            if (i == 1) Console.ForegroundColor = ConsoleColor.Red;
+            else if (i == 2) Console.ForegroundColor = ConsoleColor.Green;
+            else if (i == 3) Console.ForegroundColor = ConsoleColor.Blue;
+            else if (i == 4) Console.ForegroundColor = ConsoleColor.Gray; Console.Write(@"             
+  /$$$$$$                                                /$$$$$$                        /$$                        
+/$$__  $$                                              /$$__  $$                      | $$                        
+| $$  \ $$  /$$$$$$   /$$$$$$  /$$$$$$  /$$   /$$      | $$  \__/  /$$$$$$   /$$$$$$  /$$$$$$    /$$$$$$   /$$$$$$ 
+| $$$$$$$$ /$$__  $$ /$$__  $$|____  $$| $$  | $$      |  $$$$$$  /$$__  $$ /$$__  $$|_  $$_/   /$$__  $$ /$$__  $$
+| $$__  $$| $$  \__/| $$  \__/ /$$$$$$$| $$  | $$       \____  $$| $$  \ $$| $$  \__/  | $$    | $$$$$$$$| $$  \__/
+| $$  | $$| $$      | $$      /$$__  $$| $$  | $$       /$$  \ $$| $$  | $$| $$        | $$ /$$| $$_____/| $$      
+| $$  | $$| $$      | $$     |  $$$$$$$|  $$$$$$$      |  $$$$$$/|  $$$$$$/| $$        |  $$$$/|  $$$$$$$| $$      
+|__/  |__/|__/      |__/      \_______/ \____  $$       \______/  \______/ |__/         \___/   \_______/|__/      
+                                        /$$  | $$                                                                  
+                                       |  $$$$$$/                                                                  
+                                        \______/                                                                    
+            "); Thread.Sleep(1000);
+        }
     }
 }
