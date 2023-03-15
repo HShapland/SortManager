5ffc78fc5b457462d8cc304b3d6ace6d798dc80b
Luke Pinder <yelllukesky@gmail.com>
2023-03-15 15:40:57 +0000

    ShellSort + Tests

---
+++ SortManager/SortManagerTests/ShellSortTests.cs
@@ -0,0 +1,15 @@
+﻿using SortManager.Sorters;
+
+namespace SortManagerTests
+{
+    internal class ShellSortTests
+    {
+        [TestCase(new int[] {3, 2, 1}, new int[] {1, 2, 3})]
+        [TestCase(new int[] {9, 8, 3, 7, 5, 6, 4, 1}, new int[] {1, 3, 4, 5, 6, 7, 8, 9})]
+        public void GivenArray_ShellSort_ReturnsSortedArray(int[] input, int[] expected)
+        {
+            ShellSort sorter = new();
+            Assert.That(sorter.Sort(input), Is.EqualTo(expected));
+        }
+    }
+}
