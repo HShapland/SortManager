fddc8de131298c198ded409fe13490af683b3895
Byron Esson <byronesson767@gmail.com>
2023-03-15 14:34:12 +0000

    start count sort

---
+++ SortManager/SortManager/Sorters/CountSort.cs
@@ -0,0 +1,18 @@
+﻿namespace SortManager.Sorters;
+
+public class CountSort : Sorter
+{
+    public override string Name => "CountSort";
+
+    public override int[] Sort(int[] array)
+    {
+        int[] numCounts = new int[500];
+
+        foreach(int i in array)
+        {
+            numCounts[i - 1]++;
+        }
+
+
+    }
+}
